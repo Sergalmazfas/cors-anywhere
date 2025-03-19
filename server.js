c6da706dece86e7101652e8cca1a6f3364f6dce0
Sergalmazfas <sergalmazfas@gmail.com>
2025-03-19 13:02:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const axios = require('axios');
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.post('/chat', async (req, res) => {
